2e4179a48c2959446c910c647e7331e2437da6f8
cardevv <113392365+cardevv@users.noreply.github.com>
2022-12-15 20:09:44 -0300

    Update main.js

--- -robotron-2000-projeto_inicial/main.js
+++ -robotron-2000-projeto_inicial/main.js
@@ -1,10 +1,10 @@
-const = robotron = document.querySelector("#robotron");
+const  robotron = document.querySelector("#robotron");
 
 robotron.addEventListener("click", dizOi)
 
 
 function dizOi(){
 
-alert("oi");
+alert("Oi");
 
 }
