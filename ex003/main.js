e5f1b4ce89b18c3fb0f7aa26163d8d83273ff24e
cardevv <113392365+cardevv@users.noreply.github.com>
2022-12-15 20:37:22 -0300

    ex

---
+++ ex003/main.js
@@ -0,0 +1,11 @@
+ const botao = document.querySelector('#calcular');
+
+ const texto = document.querySelector('.resultado');
+
+ botao.addEventListener("click", function  () {
+
+    texto.innerHTML = "Fui cliclado";
+    
+     });
+
+ 
