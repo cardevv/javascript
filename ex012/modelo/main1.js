684f1272463859b9af3897ba92a470fb7f288487
cardevv <113392365+cardevv@users.noreply.github.com>
2023-01-17 21:23:26 -0300

    ex

--- ex012/modelo/main1.js
+++ ex012/modelo/main1.js
@@ -34,10 +34,71 @@
     function adicionar () {
 
         if (isNumero(num.value) && ! inLista(num.value,valores)) {
+            valores.push(Number(num.value))
+            let item = document.createElement('option')
+            item.text = `Valor ${num.value} adicionado.`
+            lista.appendChild(item)
+            res.innerHTML = ''
 
     } else {
 
         window.alert('valor invalido ou ja inserido.')
     }               
-
+            num.value = ''
+            num.focus()
               }
+
+
+              function finalizar () {
+
+                if (valores.length == 0) {
+                    window.alert('Adicione valores antes de finalizar!')
+
+                } else {
+                    let total = valores.length
+                    let maior = valores[0]
+                    let menor = valores [0]
+                    let soma = 0
+                    let media = 0
+
+                    
+                    
+
+
+                    for (let pos in valores ) {
+
+                        if (valores[pos] > maior)
+                        
+
+                            maior = valores[pos]
+
+                             if  (valores[pos] < menor) 
+
+                                menor = valores[pos]
+
+                            
+                                soma += valores[pos]
+
+                        }
+                        media = soma/ total
+
+                        res.innerHTML = ''
+                    res.innerHTML += `<p>Ao todo, temos ${total} </p>`
+                    res.innerHTML += `<p> Maior valor informado foi ${maior}. </p>`
+                    res.innerHTML += `<p> Menor valor informado foi ${menor}.  </p>`
+                    res.innerHTML += `<p>Somando todos os valores temos ${soma} </p>`
+                    res.innerHTML += `<p>A média é ${media}  </p>`
+
+
+                    }
+
+
+                    
+
+                }
+                
+               
+
+
+
+              
