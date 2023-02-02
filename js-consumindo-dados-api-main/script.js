732d538293b5c7b99bb2250b1362f91a129872fa
cardevv <113392365+cardevv@users.noreply.github.com>
2023-02-02 20:15:38 -0300

    .

--- js-consumindo-dados-api-main/script.js
+++ js-consumindo-dados-api-main/script.js
@@ -1,4 +1,6 @@
  async function buscaEndereco(cep) {
+    var campoErro = document.getElementById('erro')
+    campoErro.innerHTML = "";
     try {
         var consultaCEP = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
         var consultaCEPConvertida = await consultaCEP.json();
@@ -7,9 +9,29 @@
             throw Error ('CEP não existe!');
 
 
-        } console.log(consultaCEPConvertida)
+        }
+        var cidade = document.querySelector('#cidade')
+        var logradouro = document.querySelector('#endereco')
+        var estado = document.querySelector('#estado')
+        
+        cidade.value = consultaCEPConvertida.localidade;
+        logradouro.value = consultaCEPConvertida.logradouro;
+        estado.value = consultaCEPConvertida.uf;
+        
+        console.log(consultaCEPConvertida)
         return consultaCEPConvertida;
     } catch(erro) {
+        if (cep.length < 8 ) {
+
+            campoErro.innerHTML = `<p> Faltam digitos!</p>`
+
+        } else if (cep.length > 8) {
+            campoErro.innerHTML = `<p>Digitos a mais!</p>`
+            
+        } else {
+            campoErro.innerHTML = `<p> CEP invalido, Tente novamente!</p>`
+        }
+        
         console.log(erro)
     }
 
@@ -19,9 +41,12 @@ var cep = document.querySelector('#cep');
 cep.addEventListener("focusout", () => { buscaEndereco(cep.value)});
 
 
-let ceps = ['01001000','01001001'];
-let conjuntoCeps = ceps.map(valores => buscaEndereco(valores));
-Promise.all(conjuntoCeps).then(respostas => console.log(respostas))
+
+
+
+//let ceps = ['01001000','01001001'];
+//let conjuntoCeps = ceps.map(valores => buscaEndereco(valores));
+//Promise.all(conjuntoCeps).then(respostas => console.log(respostas))
 
 
 
