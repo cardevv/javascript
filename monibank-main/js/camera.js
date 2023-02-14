2a118540c2bd14babd72a9ade5ef2f3d6012c78f
cardevv <113392365+cardevv@users.noreply.github.com>
2023-02-14 20:31:43 -0300

    finalizando formulario começando camera

---
+++ monibank-main/js/camera.js
@@ -0,0 +1,17 @@
+const botaoIniciarCamera = document.querySelector("[data-video-botao]");
+
+const campoCamera = document.querySelector("[data-camera]");
+const video = document.querySelector("[data-camera]");
+
+
+botaoIniciarCamera.addEventListener("click", async function () {
+    const iniciarVideo = await navigator.mediaDevices.getUserMedia({video:true , audio:false})
+
+
+    botaoIniciarCamera.style.display = "none";
+    campoCamera.style.display = "block";
+
+    video.srcObject = iniciarVideo;
+
+
+})
